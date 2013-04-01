f7bcd02a84bc1370c83ceff4e483883ec22698dd
tung nguyen <tungnp92@gmail.com>
2013-04-01 18:35:52 +0700

    Initial Commit

---
+++ LayoutSubView/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  LayoutSubView
+//
+//  Created by stevie nguyen on 4/1/13.
+//  Copyright (c) 2013 tung nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ LayoutSubView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LayoutSubView
+//
+//  Created by stevie nguyen on 4/1/13.
+//  Copyright (c) 2013 tung nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
